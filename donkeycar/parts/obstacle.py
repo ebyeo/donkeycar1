65fc68028747fb918edd932a4830bc72b11be64c
Will Roscoe <ebyeo69@gmail.com>
2018-04-15 18:43:20 +0800

    enhance obstacle detection to look at last 5 readings to make sure the car is getting closer to an obstacle

--- donkeycar/parts/obstacle.py
+++ donkeycar/parts/obstacle.py
@@ -13,24 +13,69 @@ Actions are:
 
 import donkeycar.constant as Constant
 
+OBSTACLE_DISTANCE_LENGTH = 5
+
 class Obstacle():
     def __init__(self):
         # initialize default action to go forward
         self.action = Constant.OBSTACLE_ACTION_FORWARD
-    
+
+        self.front_distance_array = []
+        for i in range(OBSTACLE_DISTANCE_LENGTH):
+            self.front_distance_array = self.front_distance_array + [10000]
+        self.front_left_distance_array = []
+        for i in range(OBSTACLE_DISTANCE_LENGTH):
+            self.front_left_distance_array = self.front_left_distance_array + [10000]
+
+        self.front_right_distance_array = []
+        for i in range(OBSTACLE_DISTANCE_LENGTH):
+            self.front_right_distance_array = self.front_right_distance_array + [10000]
+
     def compute_action(self):
         if self.ultrasonic_front_distance <= 50 and self.ultrasonic_front_distance >= 0:
-            if self.ultrasonic_front_right_distance <= 50 and self.ultrasonic_front_right_distance >= 0:
-                if self.ultrasonic_front_left_distance <= 50 and self.ultrasonic_front_left_distance >= 0:
-                    return Constant.OBSTACLE_ACTION_STOP
+            stop = True
+            for i in range(OBSTACLE_DISTANCE_LENGTH-1):
+                if (self.front_distance_array[i] < self.front_distance_array[i + 1]):
+                    stop = False
+                    break
+
+            if stop:
+                return Constant.OBSTACLE_ACTION_STOP
+
+        if self.ultrasonic_front_right_distance <= 50 and self.ultrasonic_front_right_distance >= 0:
+            stop = True
+            for i in range(OBSTACLE_DISTANCE_LENGTH-1):
+                if (self.front_right_distance_array[i] < self.front_right_distance_array[i + 1]):
+                    stop = False
+                    break
+
+            if stop:
+                return Constant.OBSTACLE_ACTION_STOP
+
+        if self.ultrasonic_front_left_distance <= 50 and self.ultrasonic_front_left_distance >= 0:
+            stop = True
+            for i in range(OBSTACLE_DISTANCE_LENGTH-1):
+                if (self.front_left_distance_array[i] < self.front_left_distance_array[i + 1]):
+                    stop = False
+                    break
+
+            if stop:
+                return Constant.OBSTACLE_ACTION_STOP
 					
         return Constant.OBSTACLE_ACTION_FORWARD
 
     def run(self, img_arr=None, ultrasonic_front_distance=None, ultrasonic_front_left_distance=None, ultrasonic_front_right_distance=None):
         self.img_arr = img_arr
+
         self.ultrasonic_front_distance = ultrasonic_front_distance
+        self.front_distance_array = self.front_distance_array[1:] + [self.ultrasonic_front_distance]
+
         self.ultrasonic_front_left_distance = ultrasonic_front_left_distance
+        self.front_left_distance_array = self.front_left_distance_array[1:] + [self.ultrasonic_front_left_distance]
+
         self.ultrasonic_front_right_distance = ultrasonic_front_right_distance
+        self.front_right_distance_array = self.front_right_distance_array[1:] + [self.ultrasonic_front_right_distance]
+
         self.action = self.compute_action()
 		
         return self.action
@@ -41,5 +86,11 @@ class Obstacle():
 
 if __name__ == "__main__":
     o = Obstacle()
-    print('obstacle:', o.run(ultrasonic_front_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 55, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 54, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 53, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 62, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 51, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 50, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
+    print('obstacle:', o.run(ultrasonic_front_distance = 45, ultrasonic_front_left_distance = 55, ultrasonic_front_right_distance = 55))
 
